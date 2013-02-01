c738867f4f2c1f07afaae63ca25b4fd7fe1c900e
mh <mh@immerda.ch>
2013-02-01 13:50:47 +0100

    ensure is useless as we anyway remove the fragment if it's not managed

--- manifests/job.pp
+++ manifests/job.pp
@@ -1,18 +1,15 @@
 # watch_events: Example: create,move_to
 define dirwatcher::job(
-  $ensure = 'present',
   $watch_directory,
   $watch_events,
   $watch_command,
   $watch_recursive = true,
   $watch_autoadd = true
 ){
-  if $ensure == 'present' {
-    include ::dirwatcher
-    concat::fragment{$name:
-      content => template('dirwatcher/watcher_job.erb'),
-      order => 200,
-      target => "/etc/watcher.ini",
-    }
+  include ::dirwatcher
+  concat::fragment{$name:
+    content => template('dirwatcher/watcher_job.erb'),
+    order => 200,
+    target => "/etc/watcher.ini",
   }
 }
